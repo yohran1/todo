7f82a705f5cc9e475d5a8f6079fade4f186531fd
Yohran Kallahari <yohranv@gmail.com>
2023-06-05 20:29:56 -0300

    Primeira criação nos elementos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+
+  const [text, setText] = useState("")
+
+  function manejarMudanca(event){
+      let text = event.target.value
+      setText(text)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1>TodoList</h1>
+        <form>
+          <input onChange={manejarMudanca} type="text" placeholder="Digite o item!"></input>
+          <button>Adicionar</button>
+        </form>
+        <ul>
+          <li>{text}</li>
+        </ul>
     </div>
   );
 }
