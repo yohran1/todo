759685467b8ace0273bbe268f3a498b8d01ec378
Yohran Kallahari <yohranv@gmail.com>
2023-06-05 21:57:07 -0300

    Criando componentes e ajustando o problema do estado em componentes separados com callBack

--- src/App.js
+++ src/App.js
@@ -1,36 +1,22 @@
 
 import React, { useState } from "react"
+import Form from "./Form";
+import List from "./List";
 import './App.css';
 
 function App() {
 
-  const [text, setText] = useState("")
   const [items, setItems] = useState([])
 
-  function manejarMudanca(event){
-      let t = event.target.value
-      setText(t)
+  function onAddItem(item){
+    setItems([...items, item])
   }
-
-  function addItem(event){
-      event.preventDefault()
-      if(text){
-        setItems([...items, text])
-        setText("")
-      }
-  }
-
 
   return (
     <div className="container">
         <h1>TodoList</h1>
-        <form >
-          <input onChange={manejarMudanca} value={text} type="text" placeholder="Digite o item!"></input>
-          <button onClick={addItem}>Adicionar</button>
-        </form>
-        <ul>
-          {items.map(item => <li>{item}</li>)}
-        </ul>
+        <Form onAddItem={onAddItem}></Form>
+        <List items={items}></List>
     </div>
   );
 }
