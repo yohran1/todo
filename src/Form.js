cbc6f137d5cf6bd5076bc96ed2ed341ab0dd2a80
Yohran Kallahari <yohranv@gmail.com>
2023-06-05 22:02:21 -0300

    Adicionando Componentes

---
+++ src/Form.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react"
+import "./App.css"
+
+function Form(props){
+
+  const [text, setText] = useState("")
+
+    function manejarMudanca(event){
+        let t = event.target.value
+        setText(t)
+    }
+  
+    function addItem(event){
+        event.preventDefault()
+        if(text){
+        //   setItems([...items, text])
+          props.onAddItem(text)
+          setText("")
+        }
+    }
+
+    return(
+        <form >
+          <input onChange={manejarMudanca} value={text} type="text" placeholder="Digite o item!"></input>
+          <button onClick={addItem}>Adicionar</button>
+        </form>
+    )
+}
+export default Form
